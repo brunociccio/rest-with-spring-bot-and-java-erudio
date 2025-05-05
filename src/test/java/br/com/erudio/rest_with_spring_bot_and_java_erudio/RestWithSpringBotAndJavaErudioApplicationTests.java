854d5612e76b193fc52b9b72cd4c1cb050d3df91
Bruno Mathews de Ciccio Oliveira <boliveira@cleartech.com.br>
2025-05-05 11:59:36 -0300

    upload base do projeto base para estudo de spring bot

---
+++ src/test/java/br/com/erudio/rest_with_spring_bot_and_java_erudio/RestWithSpringBotAndJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.rest_with_spring_bot_and_java_erudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBotAndJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
