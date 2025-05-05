a4f8177a5902df114273f27f9d8f0fa9a535de1c
Bruno Mathews de Ciccio Oliveira <boliveira@cleartech.com.br>
2025-05-05 15:17:55 -0300

    teste de requisição básica

--- src/main/java/br/com/erudio/Startup.java
+++ src/main/java/br/com/erudio/Startup.java
@@ -1,4 +1,4 @@
-package br.com.erudio.rest_with_spring_bot_and_java_erudio;
+package br.com.erudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Startup {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Startup.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Startup.class, args);
+    }
 }
